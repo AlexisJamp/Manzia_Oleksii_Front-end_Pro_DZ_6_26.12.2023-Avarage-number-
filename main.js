4e5ef062ca77fdce2c74c08e4c9aac980475d632
AlexisJamp <154271476+AlexisJamp@users.noreply.github.com>
2024-01-03 10:47:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let firstNumber = Number(prompt("Enter first number: "));
+let secondNumber = Number(prompt("Enter second number: "));
+let thirdNumber = Number(prompt("Enter third number: "));
+
+let avarage = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(`Avarage is: ${avarage}!`)
